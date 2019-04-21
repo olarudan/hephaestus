4cfe4635ec0c124dd25a9ff3ca59623f68bf0cfc
Dan Olaru <dan.olaru@reisystems.com>
2019-04-21 17:35:43 -0400

    Add the config class that handles configuration of the app

--- hephaestus/config.py
+++ hephaestus/config.py
@@ -1,12 +1,17 @@
-import sys, os, yaml
+import sys
+import os
+import yaml
 
 class Config:
   def __init__(self):
     dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+    print dir
 
-    # get general config
-    with open(os.path.join(dir, "config.yml"), 'r') as ymlfile:
-      self.cfg = yaml.load(ymlfile)
+    # load config from yml file
+    with open(os.path.join(dir, "config.yml"), 'r') as yml_file:
+      self.cfg = yaml.load(yml_file)
 
   def get_config(self):
     return self.cfg
+
+config = Config().get_config()

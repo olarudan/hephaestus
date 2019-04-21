97d8a2d2303edb4f6f2ffbcd2507f02975329d22
Dan Olaru <dan.olaru@reisystems.com>
2019-04-21 17:37:22 -0400

    Re-work setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
+from setuptools import setup
+
 setup(
     scripts=['bin/hephaestus'],
 )
+
+setup(name='hephaestus',
+      version='0.1',
+      description='Hephaestus rudimentary configuration management tool',
+      url='https://github.com/olarudan/hephaestus',
+      author='Dan Olaru',
+      author_email='olarudan@gmail.com',
+      license='MIT',
+      packages=['hephaestus'],
+      zip_safe=False)

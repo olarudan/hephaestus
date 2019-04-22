c7db4a03854f122a57de53c3ab182c987305559c
Dan Olaru <dan.olaru@reisystems.com>
2019-04-21 21:16:23 -0400

    Add SSH class that takes care of sending commands to hosts over ssh connections

--- hephaestus/ssh.py
+++ hephaestus/ssh.py
@@ -1,1 +1,23 @@
-#import paramiko
+from hephaestus.config import config
+import paramiko
+
+class SSH:
+  def __init__(self, hostname):
+    self.hostname = hostname
+    self.username = config['ssh']['username'] 
+    self.password = config['ssh']['password']
+
+  def connect(self):
+    try:
+      self.ssh_client = paramiko.SSHClient()
+      self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+      self.ssh_client.connect(hostname=self.hostname, username=self.username, password=self.password)
+      # log successfull connection
+    except:
+      #log something bad here
+      print('something went wrong')
+
+  def execute(self, command):
+    stdin,stdout,stderr=self.ssh_client.exec_command(command)
+    print(stdout.readlines())
+    # log here

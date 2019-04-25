5611c8992b8f88a07c6beedfb5047dcf7b39125f
Dan Olaru <dan.olaru@reisystems.com>
2019-04-25 15:33:20 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from setuptools import setup
 
 setup(
+    name='hephaestus',
     scripts=['bin/hep'],
-)
-
-setup(name='hephaestus',
-      version='0.1',
-      description='Hephaestus rudimentary configuration management tool',
-      url='https://github.com/olarudan/hephaestus',
-      author='Dan Olaru',
-      author_email='olarudan@gmail.com',
-      license='MIT',
-      packages=['hephaestus'],
-      zip_safe=False)
+    version='0.1',
+    description='Hephaestus rudimentary configuration management tool',
+    url='https://github.com/olarudan/hephaestus',
+    author='Dan Olaru',
+    author_email='olarudan@gmail.com',
+    license='MIT',
+    packages=['hephaestus'],
+    zip_safe=False)

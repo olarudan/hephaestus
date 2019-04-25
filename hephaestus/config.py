6209be193697851f17248bc2006badb38ba63275
Dan Olaru <dan.olaru@reisystems.com>
2019-04-25 00:10:26 -0400

    Override config params with cli params

--- hephaestus/config.py
+++ hephaestus/config.py
@@ -1,16 +1,38 @@
 import sys
 import os
 import yaml
+import argparse
 
 class Config:
   """ A class used to manage configuration for the hephaestus configuration management tool. """
 
   def __init__(self):
-    dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+    
+    # parse cli arguments
+    self.parser = argparse.ArgumentParser(description = 'Execute hep manifests against remote hosts')
+    self.parser.add_argument('manifest', nargs='*',  help = 'Manifest file to run')
+    self.parser.add_argument('--hosts', '-i', help = 'Hosts file to use')
+    self.parser.add_argument('--config', '-c', help = 'Config file to use')
+    self.args = self.parser.parse_args()
+
+    # override config file path if passed through clid
+    if (self.args.config == None):
+      dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+      config_file_path = os.path.join(dir, "config.yml")
+    else:
+      config_file_path = os.path.realpath(self.args.config)
 
     # load config from yml file
-    with open(os.path.join(dir, "config.yml"), 'r') as yml_file:
+    with open(config_file_path, 'r') as yml_file:
       self.cfg = yaml.load(yml_file)
+
+    # override manifest file path if passed through cli
+    if (self.args.manifest != []):
+      self.cfg['manifest'] = self.args.manifest[0]
+
+    # override hosts file path if passed through cli
+    if (self.args.hosts != None):
+      self.cfg['hosts'] = self.args.hosts
 
   def get_config(self):
     """ Returns hepahestus configuration.
